869c4747e324837c0808f43a1af84ee33d91be90
Antonin-WOZNIAK <antonin.wozniak@edu.univ-fcomte.fr>
2024-11-08 14:45:55 +0100

    Collections d'éléments
    Trouvez tous les 'H2' du document
    Puis pour chacun, suite à un click, mettre la cible de l'événement en rouge.

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,10 @@ const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click") ;
 enRougeSuiteAClick.addEventListener("click", (evt) =>{
     enRougeSuiteAClick.style.color = "red";
 });
+
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((collH2)=>{
+    collH2.addEventListener("click", (evt)=>{
+        evt.target.style.color = "red" ;
+    });
+});
