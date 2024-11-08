3024108292d2253dbc6abeea3910e6f937e83204
Antonin-WOZNIAK <antonin.wozniak@edu.univ-fcomte.fr>
2024-11-08 14:39:35 +0100

    Simples interactions
    Changer la couleur du texte et Réagir à un click

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 // TODO
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge") ;
+aMettreEnRouge.style.color = "red" ;
+
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click") ;
+enRougeSuiteAClick.addEventListener("click", (evt) =>{
+    enRougeSuiteAClick.style.color = "red";
+});
